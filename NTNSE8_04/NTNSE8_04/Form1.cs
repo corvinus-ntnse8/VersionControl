c3e3532c442f5ba8662f6fb1ec5d74acbb124ae0
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-10-06 19:42:30 +0200

    adatbetoltes

--- NTNSE8_04/NTNSE8_04/Form1.cs
+++ NTNSE8_04/NTNSE8_04/Form1.cs
@@ -12,9 +12,17 @@ namespace NTNSE8_04
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        public  void LoadData()
+        {
+           Flats = context.Flat.ToList();
         }
     }
 }
