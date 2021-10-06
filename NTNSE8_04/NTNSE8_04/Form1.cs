e15337c4726658b70bbb1a24f1a92f46ca301a50
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-10-06 19:58:48 +0200

    munkafuzet letrehozas

--- NTNSE8_04/NTNSE8_04/Form1.cs
+++ NTNSE8_04/NTNSE8_04/Form1.cs
@@ -16,15 +16,46 @@ namespace NTNSE8_04
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         public  void LoadData()
         {
            Flats = context.Flat.ToList();
+        }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 }
