f43a80c43bf144925f6eee8fab7cc66650854363
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-10-20 14:46:53 +0200

    write to file

--- NTNSE8_week06/NTNSE8_week06/Form1.cs
+++ NTNSE8_week06/NTNSE8_week06/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,13 +17,14 @@ namespace NTNSE8_week06
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<decimal> Nyereségek = new List<decimal>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Tick.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
-            List<decimal> Nyereségek = new List<decimal>();
+            
             int intervalum = 30;
             DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
             DateTime záróDátum = new DateTime(2016, 12, 30);
@@ -68,6 +70,24 @@ namespace NTNSE8_week06
                 value += last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            using (var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var ny in Nyereségek)
+                {
+                    sw.WriteLine(string.Format(
+                        "{0}",
+                        ny));
+                }
+            }
         }
     }
 
