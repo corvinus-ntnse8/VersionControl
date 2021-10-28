3e75e84e0d9faa637fc35d6e7c597d3a1a92761a
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-10-28 10:38:54 +0200

    chart

--- NTNSE8_hatodik/NTNSE8_hatodik/Form1.cs
+++ NTNSE8_hatodik/NTNSE8_hatodik/Form1.cs
@@ -9,17 +9,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace NTNSE8_hatodik
 {
     public partial class Form1 : Form
     {
-        BindingList<RateData> Rates;
-        string result;
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
+            WebServiceHivo();
+            dataGridView1.DataSource = Rates;
+            XMLFeldolgozo();
+            Diagramkeszito();                   
+        }
+
+        private string WebServiceHivo()
+        {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
@@ -27,18 +35,14 @@ namespace NTNSE8_hatodik
                 startDate = "2020-01-01",
                 endDate = "2020-06-30"
             };
-
             var response = mnbService.GetExchangeRates(request);
-            result = response.GetExchangeRatesResult;
-
-            dataGridView1.DataSource = Rates;
-            XMLFeldolgozo();
-
+            var result = response.GetExchangeRatesResult;
+            return result;
         }
         private void XMLFeldolgozo()
         {
             var xml = new XmlDocument();
-            xml.LoadXml(result);
+            xml.LoadXml(WebServiceHivo());
 
             
             foreach (XmlElement element in xml.DocumentElement)
@@ -56,6 +60,23 @@ namespace NTNSE8_hatodik
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+        }
+        private void Diagramkeszito()
+        {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
