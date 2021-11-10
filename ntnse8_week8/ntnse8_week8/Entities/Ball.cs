4f506fb5796b9fbc836016a72f4c95b0f0effee0
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-11-10 19:41:12 +0100

    abstract a Ballban

--- ntnse8_week8/ntnse8_week8/Entities/Ball.cs
+++ ntnse8_week8/ntnse8_week8/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ntnse8_week8.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,28 +9,11 @@ using System.Windows.Forms;
 
 namespace ntnse8_week8.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
+        protected override void DrawImage(Graphics g)
         {
-            AutoSize = false;
-            Width = 50;
-            Height = 50;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
-        {
-            g.FillEllipse(new SolidBrush(Color.Blue),0, 0, Width, Height);
-        }
-        public void MoveBall()
-        {
-            Left += 1;
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
     }
 }
