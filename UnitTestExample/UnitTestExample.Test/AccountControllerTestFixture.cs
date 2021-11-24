121bb8da10b75075ac47e782068a95d1d9e00f24
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-11-24 20:28:59 +0100

    register kivetel

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,7 +44,7 @@ namespace UnitTestExample.Test
            Test,
            TestCase("irf@uni - corvinus.hu", "Abcd1234"),
            TestCase("irf@uni-corvinus.hu", "Abcd1234567")
-       ]
+        ]
         public void TestRegisterHappyPath(string email, string password)
         {
             var accountController = new AccountController();
@@ -52,7 +53,30 @@ namespace UnitTestExample.Test
             Assert.AreEqual(password, actresult.Password);
             Assert.AreNotEqual(Guid.Empty, actresult.ID);
         }
+
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus", "Abcd1234"),
+            TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+            TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+            TestCase("irf@uni-corvinus.hu", "Ab1234"),
+        ]
+
+        public void TestRegisterValidateException(string email, string password)
+        {
+            var accountController = new AccountController();
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
+        }
     }
-
-
 }
