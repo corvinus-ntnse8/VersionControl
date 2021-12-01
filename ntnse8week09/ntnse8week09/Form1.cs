2778b2e854a5fafa20e2f5b0d7e4026d3f683ae9
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-12-01 17:35:33 +0100

    generate random

--- ntnse8week09/ntnse8week09/Form1.cs
+++ ntnse8week09/ntnse8week09/Form1.cs
@@ -14,6 +14,7 @@ namespace ntnse8week09
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
@@ -42,7 +43,6 @@ namespace ntnse8week09
                     });
                 }
             }
-
             return population;
         }
 
