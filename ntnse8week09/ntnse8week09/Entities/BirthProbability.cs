79dc108f9196727c98336b57407ca8953a448244
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-12-01 17:10:46 +0100

    create classes

---
+++ ntnse8week09/ntnse8week09/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ntnse8week09.Entities
+{
+    public class BirthProbability
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double SzulVal { get; set; }
+    }
+}
---
+++ ntnse8week09/ntnse8week09/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ntnse8week09.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int BirthYear { get; set; }
+        public double HalVal { get; set; }
+    }
+}
