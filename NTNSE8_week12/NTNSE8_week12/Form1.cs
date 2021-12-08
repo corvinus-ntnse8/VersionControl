d1dff102f1ec6d47c3eaeabf684407af375d34a2
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-12-08 19:28:16 +0100

    iteracios lepes felepitese

--- NTNSE8_week12/NTNSE8_week12/Form1.cs
+++ NTNSE8_week12/NTNSE8_week12/Form1.cs
@@ -25,11 +25,24 @@ namespace NTNSE8_week12
             ga = gc.ActivateDisplay();
             //gc.AddPlayer();
             //gc.Start(true);
+            gc.GameOver += Gc_GameOver;
             for (int i = 0; i <= populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
-            }         
+            }
+            gc.Start();
             this.Controls.Add(ga);
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format("{0}. generáció", generation);
+            label1.BringToFront();
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
         }
     }
 }
